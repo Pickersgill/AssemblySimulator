11b10e38bed5c7f543c3985557c0f860d6049862
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2021-01-17 16:33:17 +0000

    Moved variable memory assignment stuff to Assembler.
    This amends a previous design error.

--- assembler.py
+++ assembler.py
@@ -22,6 +22,11 @@ class Instruction:
 class Assembler:
 	
 	def __init__(self, file_src):
+		self.MEM_START = 0
+		self.MEM_SIZE = 200
+
+		self.var_table = {}
+
 		self.lines = []
 		content = open(file_src)
 		
@@ -37,9 +42,29 @@ class Assembler:
 
 	def make_instruction(self, line):
 		instruction = self.get_instr(line)
-		args = line[1:]
+		args = self.assemble_args(line[1:])
 		return Instruction([instruction] + args)
 
+
+	def assemble_args(self, arg_list):
+		for i, arg in enumerate(arg_list):
+			if not arg.isnumeric():
+				arg_list[i] = self.get_var_mem_loc(arg_list[i])
+			else:
+				arg_list[i] = arg
+
+		return arg_list
+
+	def get_var_mem_loc(self, var_name):
+		if var_name in self.var_table.keys():
+			return self.var_table[var_name]
+		elif self.var_table.values():
+			self.var_table[var_name] = max(self.var_table.values()) + 1
+		else:
+			self.var_table[var_name] = self.MEM_START
+
+		return self.get_var_mem_loc(var_name)
+	
 
 	def get_instr(self, instruction):
 		return INSTR_MAP[instruction[0]]
--- simulator.py
+++ simulator.py
@@ -6,14 +6,14 @@ import numpy as np
 class Simulator:
 	
 	def __init__(self, ass, slow=False):
-		self.MEM_START = 0
-		self.MEM_SIZE = 200
+		self.MEM_START = ass.MEM_START
+		self.MEM_SIZE = ass.MEM_SIZE
 		self.PC = 0
 		self.PROGRAMME_LEN = len(ass.lines)
 
 		self.slow = slow
 
-		self.var_table = {}
+		self.var_table = ass.var_table
 		
 		self.func_map = dict(enumerate([self.set, 
 			self.beq, self.add, self.sub, self.mul, self.div], start=1))
@@ -37,18 +37,6 @@ class Simulator:
 			print(line)
 			args = line.args
 			self.func_map[line.instruction_code](args)
-		
-
-	def get_var_mem_loc(self, var_name):
-		if var_name in self.var_table.keys():
-			return self.var_table[var_name]
-		elif self.var_table.values():
-			self.var_table[var_name] = max(self.var_table.values()) + 1
-		else:
-			self.var_table[var_name] = self.MEM_START
-
-		return self.get_var_mem_loc(var_name)
-		
 	
 	def init_memory(self):
 		self.memory = np.zeros(self.MEM_SIZE, dtype=int)
@@ -57,10 +45,10 @@ class Simulator:
 		self.memory[int(location)] = value
 
 	def mc(self, loc):
-		return self.memory[self.get_var_mem_loc(loc)]
+		return self.memory[loc]
 
 	def set(self, args):
-		dst = self.get_var_mem_loc(args[0])
+		dst = args[0]
 		k = args[1]
 		self.set_mem(dst, k)
 		self.PC += 1
@@ -77,7 +65,7 @@ class Simulator:
 		pass
 
 	def add(self, args):
-		dst = self.get_var_mem_loc(args[0])
+		dst = args[0]
 		src1 = self.mc(args[1])
 		src2 = self.mc(args[2])
 		self.set_mem(dst, src1 + src2)
@@ -85,7 +73,7 @@ class Simulator:
 		pass
 
 	def sub(self, args):
-		dst = self.get_var_mem_loc(args[0])
+		dst = args[0]
 		src1 = self.mc(args[1])
 		src2 = self.mc(args[2])
 		self.set_mem(dst, src1 - src2)
@@ -93,7 +81,7 @@ class Simulator:
 		pass
 
 	def mul(self, args):
-		dst = self.get_var_mem_loc(args[0])
+		dst = args[0]
 		src1 = self.mc(args[1])
 		src2 = self.mc(args[2])
 		self.set_mem(dst, src1 * src2)
@@ -101,7 +89,7 @@ class Simulator:
 		pass
 
 	def div(self, args):
-		dst = self.get_var_mem_loc(args[0])
+		dst = args[0]
 		src1 = self.mc(args[1])
 		src2 = self.mc(args[2])
 		self.set_mem(dst, src1 // src2)
